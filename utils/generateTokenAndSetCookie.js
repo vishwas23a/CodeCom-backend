970021e71d152776d0e07f2ee675479ff0d5aa87
Vishwas Kumar Verma <vishwasverma23@gmail.com>
2024-12-24 22:02:14 +0530

    fixed

--- utils/generateTokenAndSetCookie.js
+++ utils/generateTokenAndSetCookie.js
@@ -8,9 +8,10 @@ res.cookie("authToken",token,{
 
     httpOnly:true,
     secure:process.env.NODE_ENV==='production',
-    sameSite:"strict",
+    sameSite:"none",
     maxAge: 7*24*60*60*1000
 })
 return token
 
 }
+    
